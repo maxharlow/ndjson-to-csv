0dc1fb98891a101a32fa9082a1360f5ceb919127
Max Harlow <maxharlow@gmail.com>
2018-01-27 15:48:20 +0000

    Bit of a refactor

--- ndjson-to-csv.js
+++ ndjson-to-csv.js
@@ -7,30 +7,18 @@ const NDJson = require('ndjson')
 const Flat = require('flat')
 const CSVWriter = require('csv-write-stream')
 
-function setup() {
+async function setup() {
     const interface = Yargs
         .usage('Usage: ndjson-to-csv [filename]')
         .wrap(null)
-        .demandCommand(1, '')
         .help('?').alias('?', 'help')
         .version().alias('v', 'version')
-    run(interface.argv._[0])
-}
-
-async function run(input) {
-    const filename = input === undefined || input === '-' ? '/dev/stdin' : input
     try {
-        if (Process.stdin.isTTY === true && filename === '/dev/stdin') throw new Error('Error: no input')
-        console.error('Starting up...')
-        const total = await length(filename)
-        const headersProgress = new Progress('Detecting headers [:bar] :percent / :etas left', { total, width: 50 })
-        const headersTick = () => headersProgress.tick()
-        const headers = await processHeaders(filename, headersTick)
-        const bodyProgress = new Progress('Writing data      [:bar] :percent / :etas left', { total, width: 50 })
-        const bodyTick = () => bodyProgress.tick()
-        processBody(filename, headers, bodyTick)
-            .through(CSVWriter())
-            .pipe(Process.stdout)
+        const input = interface.argv._[0]
+        if (input === undefined && Process.stdin.isTTY) return interface.showHelp()
+        if (input === undefined || input === '-') throw new Error('Error: reading from standard input not yet supported')
+        const output = await run(input)
+        output.pipe(CSVWriter()).pipe(Process.stdout)
     }
     catch (e) {
         console.error(e.message)
@@ -38,34 +26,47 @@ async function run(input) {
     }
 }
 
-function length(filename) {
-    return Highland(FS.createReadStream(filename))
-        .through(NDJson.parse())
-        .reduce(0, a => a + 1)
-        .toPromise(Promise)
+async function run(filename) {
+    console.error('Starting up...')
+    const total = await length(read(filename))
+    const headersData = read(filename)
+    headersData.observe().each(ticker('Detecting headers', total))
+    const headers = await detectHeaders(headersData)
+    const bodyData = read(filename)
+    bodyData.observe().each(ticker('Writing data     ', total))
+    const body = processBody(bodyData, headers)
+    return body
 }
 
-function processHeaders(filename, tick) {
-    return Highland(FS.createReadStream(filename))
-        .through(NDJson.parse())
+function ticker(text, total) {
+    const progress = new Progress(text + ' [:bar] :percent / :etas left', { total, width: 50 })
+    return () => progress.tick()
+}
+
+function read(filename) {
+    return Highland(FS.createReadStream(filename)).through(NDJson.parse())
+}
+
+function length(input) {
+    return input.reduce(0, a => a + 1).toPromise(Promise)
+}
+
+function detectHeaders(input) {
+    return input
         .reduce([], (a, row) => {
-            tick()
             const keys = Object.keys(Flat(row))
             return Array.from(new Set(a.concat(keys)))
         })
         .toPromise(Promise)
 }
 
-function processBody(filename, headers, tick) {
-    return Highland(FS.createReadStream(filename))
-        .through(NDJson.parse())
-        .map(row => {
-            tick()
-            const rowFlat = Flat(row)
-            return headers.slice().reverse().reduce((a, header) => {
-                return Object.assign({ [header]: rowFlat[header] }, a)
-            }, {})
-        })
+function processBody(input, headers) {
+    return input.map(row => {
+        const rowFlat = Flat(row)
+        return headers.slice().reverse().reduce((a, header) => {
+            return Object.assign({ [header]: rowFlat[header] }, a)
+        }, {})
+    })
 }
 
 setup()
